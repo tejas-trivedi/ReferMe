3662d8a925e59425af8a7392e35fd8991604b5f2
tejas-trivedi <500077185@stu.upes.ac.in>
2022-03-15 23:40:31 +0530

    home/login/register

---
+++ lib/services/database_service.dart
@@ -0,0 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+
+  final String uid;
+  DatabaseService({
+    this.uid
+  });
+
+  final CollectionReference userCollection = Firestore.instance.collection('users');
+  final CollectionReference groupCollection = Firestore.instance.collection('groups');
+
+  Future updateUserData(String fullName, String email, String password) async {
+    return await userCollection.document(uid).setData({
+      'fullName': fullName,
+      'email': email,
+      'password': password,
+      'groups': [],
+      'profilePic': ''
+    });
+  }
+
+  Future createGroup(String userName, String groupName) async {
+    DocumentReference groupDocRef = await groupCollection.add({
+      'groupName': groupName,
+      'groupIcon': '',
+      'admin': userName,
+      'members': [],
+      'groupId': '',
+      'recentMessage': '',
+      'recentMessageSender': ''
+    });
+
+    await groupDocRef.updateData({
+        'members': FieldValue.arrayUnion([uid + '_' + userName]),
+        'groupId': groupDocRef.documentID
+    });
+
+    DocumentReference userDocRef = userCollection.document(uid);
+    return await userDocRef.updateData({
+      'groups': FieldValue.arrayUnion([groupDocRef.documentID + '_' + groupName])
+    });
+  }
+
+
+  Future togglingGroupJoin(String groupId, String groupName, String userName) async {
+
+    DocumentReference userDocRef = userCollection.document(uid);
+    DocumentSnapshot userDocSnapshot = await userDocRef.get();
+
+    DocumentReference groupDocRef = groupCollection.document(groupId);
+
+    List<dynamic> groups = await userDocSnapshot.data['groups'];
+
+    if(groups.contains(groupId + '_' + groupName)) {
+      await userDocRef.updateData({
+        'groups': FieldValue.arrayRemove([groupId + '_' + groupName])
+      });
+
+      await groupDocRef.updateData({
+        'members': FieldValue.arrayRemove([uid + '_' + userName])
+      });
+    }
+    else {
+      await userDocRef.updateData({
+        'groups': FieldValue.arrayUnion([groupId + '_' + groupName])
+      });
+
+      await groupDocRef.updateData({
+        'members': FieldValue.arrayUnion([uid + '_' + userName])
+      });
+    }
+  }
+
+  Future<bool> isUserJoined(String groupId, String groupName, String userName) async {
+
+    DocumentReference userDocRef = userCollection.document(uid);
+    DocumentSnapshot userDocSnapshot = await userDocRef.get();
+
+    List<dynamic> groups = await userDocSnapshot.data['groups'];
+
+    
+    if(groups.contains(groupId + '_' + groupName)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  Future getUserData(String email) async {
+    QuerySnapshot snapshot = await userCollection.where('email', isEqualTo: email).getDocuments();
+    print(snapshot.documents[0].data);
+    return snapshot;
+  }
+
+
+  getUserGroups() async {
+    return Firestore.instance.collection("users").document(uid).snapshots();
+  }
+
+
+  // send message
+  sendMessage(String groupId, chatMessageData) {
+    Firestore.instance.collection('groups').document(groupId).collection('messages').add(chatMessageData);
+    Firestore.instance.collection('groups').document(groupId).updateData({
+      'recentMessage': chatMessageData['message'],
+      'recentMessageSender': chatMessageData['sender'],
+      'recentMessageTime': chatMessageData['time'].toString(),
+    });
+  }
+
+
+  getChats(String groupId) async {
+    return Firestore.instance.collection('groups').document(groupId).collection('messages').orderBy('time').snapshots();
+  }
+
+  searchByName(String groupName) {
+    return Firestore.instance.collection("groups").where('groupName', isEqualTo: groupName).getDocuments();
+  }
+}
